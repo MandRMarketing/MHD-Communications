bb19dc3a57f20c0ea693c15bea24b0ef9015687b
Logan Scheid <scheid.logan@gmail.com>
2024-09-16 11:51:52 -0400

    old footer fix

--- wp-content/themes/mhd/footer.php
+++ wp-content/themes/mhd/footer.php
@@ -1,3 +1,8 @@
+<?php
+$column_left = get_option('options_footer_column_left');
+$column_center = get_option('options_footer_column_center');
+$column_right = get_option('options_footer_column_right');
+?>
 </div>
 <!-- <footer id="footer" class="footer">
 	<div class="footer__primary">
@@ -19,32 +24,14 @@
 	<div class="footer__primary">
 		<div class="footer__primary__container container">
 			<div class="footer__primary__container__left">
-				<h3>Contact</h3>
-				<a href="tel:833-643-4636">P: 833-MHD-INFO</a>
-				<a href="tel:833-643-4636">P: 833-643-4636</a>
-				<a href="tel:813-699-5001">F: 813-699-5001</a>
-				<a href="mailto:info@mhdit.com">info@mhdit.com</a>
-				<a style="margin-top: 35px;" class="button-arrow" target="_blank" href="https://mhdit.hostedrmm.com:8040/ ">Initiate Remote Support</a>
+				<?= $column_left ?>
 				<?php get_template_part('template-parts/social-links'); ?>
 			</div>
 			<div class="footer__primary__container__center">
-				<h3>Locations</h3>
-				<a target="_blank" href="https://maps.app.goo.gl/n9teBxqS9Yjbm7he8">5808 Breckenridge Pkwy<br />Suite G<br />Tampa, FL 33610</a>
-				<br />
-				<a target="_blank" href="https://maps.app.goo.gl/v4eGEQEi44RLkXYy9">1750 N. Florida Mango Road<br />Suite 413<br />West Palm Beach, FL 33409</a>
+				<?= $column_center ?>
 			</div>
 			<div class="footer__primary__container__right">
-				<h3>Quicklinks</h3>
-				<a class="button-arrow" target="_blank" href="https://mhdit.myportallogin.com/">Portal Login</a>
-				<a class="button-arrow" href="/services/managed-it-services/">Managed IT Services</a>
-				<a class="button-arrow" href="/services/managed-it-security/">Managed IT Security</a>
-				<a class="button-arrow" href="/services/cabling-services/">Network Cabling Services</a>
-				<a class="button-arrow" href="/services/audio-visual-systems/">Audio/Visual Systems </a>
-				<a class="button-arrow" href="/services/access-control-systems/">Access Control Systems</a>
-				<a class="button-arrow" href="/services/surveillance-systems/">Surveillance Systems</a>
-				<a class="button-arrow" href="/services/phone-systems/">Phone Systems </a>
-				<a class="button-arrow" href="/services/office-moving-services/">Office Moving Services</a>
-				<a class="button-arrow" href="/services/wireless-survey-services/">Wireless Site Surveys </a>
+				<?= $column_right ?>
 			</div>
 		</div>
 	</div>
